1219986b2d35872d17fc45ab2a11aa0d66d73196
nekomimi-daimao <>
2019-02-20 23:23:16 +0900

    define exception

---
+++ Assets/PUN2Rx/Plugins/PUN2Exception.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace PUN2Rx
+{
+    public class PUN2Exception : Exception
+    {
+        public short ErrorCode { get; private set; }
+
+        public string ErrorMessage { get; private set; }
+
+        public PUN2Exception(short errorCode, string errorMessage)
+        {
+            this.ErrorCode = errorCode;
+            this.ErrorMessage = errorMessage;
+        }
+
+        public PUN2Exception() : base()
+        {
+        }
+
+        public PUN2Exception(string message) : base(message)
+        {
+        }
+
+        public PUN2Exception(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected PUN2Exception(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
