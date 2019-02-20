5dc3c60152a4e93d9b3bc0008759f03a2ea3f67e
nekomimi-daimao <>
2019-02-21 01:44:54 +0900

    implement IInRoomCallbacks

---
+++ Assets/PUN2Rx/Plugins/Triggers/InRoomCallbacksTriggers.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Photon.Pun;
+using Photon.Realtime;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+
+namespace PUN2Rx
+{
+    [DisallowMultipleComponent]
+    public class InRoomCallbacksTriggers : ObservableTriggerBase, IInRoomCallbacks
+    {
+        #region PUNCallbacks
+
+        private Subject<Player> onPlayerEnteredRoom;
+
+        public void OnPlayerEnteredRoom(Player newPlayer)
+        {
+            onPlayerEnteredRoom?.OnNext(newPlayer);
+        }
+
+        public IObservable<Player> OnPlayerEnteredRoomAsObservable()
+        {
+            return onPlayerEnteredRoom ?? (onPlayerEnteredRoom = new Subject<Player>());
+        }
+
+        private Subject<Player> onPlayerLeftRoom;
+
+        public void OnPlayerLeftRoom(Player otherPlayer)
+        {
+            onPlayerLeftRoom?.OnNext(otherPlayer);
+        }
+
+        public IObservable<Player> OnPlayerLeftRoomAsObservable()
+        {
+            return onPlayerLeftRoom ?? (onPlayerLeftRoom = new Subject<Player>());
+        }
+
+        private Subject<Hashtable> onRoomPropertiesUpdate;
+
+        public void OnRoomPropertiesUpdate(Hashtable propertiesThatChanged)
+        {
+            onRoomPropertiesUpdate?.OnNext(propertiesThatChanged);
+        }
+
+        public IObservable<Hashtable> OnRoomPropertiesUpdateAsObservable()
+        {
+            return onRoomPropertiesUpdate ?? (onRoomPropertiesUpdate = new Subject<Hashtable>());
+        }
+
+        private Subject<Tuple<Player, Hashtable>> onPlayerPropertiesUpdate;
+
+        public void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
+        {
+            onPlayerPropertiesUpdate?.OnNext(Tuple.Create(targetPlayer, changedProps));
+        }
+
+        public IObservable<Tuple<Player, Hashtable>> OnPlayerPropertiesUpdateAsObservable()
+        {
+            return onPlayerPropertiesUpdate ?? (onPlayerPropertiesUpdate = new Subject<Tuple<Player, Hashtable>>());
+        }
+
+        private Subject<Player> onMasterClientSwitched;
+
+        public void OnMasterClientSwitched(Player newMasterClient)
+        {
+            onMasterClientSwitched?.OnNext(newMasterClient);
+        }
+
+        public IObservable<Player> onMasterClientSwitchedAsObservable()
+        {
+            return onMasterClientSwitched ?? (onMasterClientSwitched = new Subject<Player>());
+        }
+
+        #endregion
+
+        #region lifecycle
+
+        private void OnEnable()
+        {
+            PhotonNetwork.AddCallbackTarget(this);
+        }
+
+        private void OnDisable()
+        {
+            PhotonNetwork.RemoveCallbackTarget(this);
+        }
+
+        #endregion
+
+        #region UniRx
+
+        protected override void RaiseOnCompletedOnDestroy()
+        {
+            onPlayerEnteredRoom?.OnCompleted();
+            onPlayerLeftRoom?.OnCompleted();
+            onRoomPropertiesUpdate?.OnCompleted();
+            onPlayerPropertiesUpdate?.OnCompleted();
+            onMasterClientSwitched?.OnCompleted();
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/PUN2Rx/Plugins/Triggers/InRoomCallbacksTriggersExtension.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Photon.Realtime;
+using UniRx;
+using UnityEngine;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+
+namespace PUN2Rx
+{
+    public static class InRoomCallbacksTriggersExtension
+    {
+        public static IObservable<Player> OnPlayerEnteredRoom(this Component component)
+        {
+            return component?.gameObject == null
+                ? Observable.Empty<Player>()
+                : GetOrAddComponent<InRoomCallbacksTriggers>(component.gameObject).OnPlayerEnteredRoomAsObservable();
+        }
+
+        public static IObservable<Player> OnPlayerLeftRoom(this Component component)
+        {
+            return component?.gameObject == null
+                ? Observable.Empty<Player>()
+                : GetOrAddComponent<InRoomCallbacksTriggers>(component.gameObject).OnPlayerLeftRoomAsObservable();
+        }
+
+        public static IObservable<Hashtable> OnRoomPropertiesUpdate(this Component component)
+        {
+            return component?.gameObject == null
+                ? Observable.Empty<Hashtable>()
+                : GetOrAddComponent<InRoomCallbacksTriggers>(component.gameObject).OnRoomPropertiesUpdateAsObservable();
+        }
+
+        public static IObservable<Tuple<Player, Hashtable>> OnPlayerPropertiesUpdate(this Component component)
+        {
+            return component?.gameObject == null
+                ? Observable.Empty<Tuple<Player, Hashtable>>()
+                : GetOrAddComponent<InRoomCallbacksTriggers>(component.gameObject).OnPlayerPropertiesUpdateAsObservable();
+        }
+
+        public static IObservable<Player> OnMasterClientSwitched(this Component component)
+        {
+            return component?.gameObject == null
+                ? Observable.Empty<Player>()
+                : GetOrAddComponent<InRoomCallbacksTriggers>(component.gameObject).onMasterClientSwitchedAsObservable();
+        }
+
+        private static T GetOrAddComponent<T>(GameObject gameObject)
+            where T : Component
+        {
+            var component = gameObject.GetComponent<T>();
+            if (component == null)
+            {
+                component = gameObject.AddComponent<T>();
+            }
+
+            return component;
+        }
+    }
+}
