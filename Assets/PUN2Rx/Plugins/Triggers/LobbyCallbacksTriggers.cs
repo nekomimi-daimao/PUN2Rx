37f03e38a82792c4736bdc882958e07f679619b2
nekomimi-daimao <>
2019-02-21 02:12:55 +0900

    implement ILobbyCallback

---
+++ Assets/PUN2Rx/Plugins/Triggers/LobbyCallbacksTriggers.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace PUN2Rx
+{
+    [DisallowMultipleComponent]
+    public class LobbyCallbacksTriggers : ObservableTriggerBase, ILobbyCallbacks
+    {
+        private Subject<Unit> onJoinedLobby;
+
+        public void OnJoinedLobby()
+        {
+            onJoinedLobby?.OnNext(Unit.Default);
+        }
+
+        public IObservable<Unit> OnJoinedLobbyAsObservable()
+        {
+            return onJoinedLobby ?? (onJoinedLobby = new Subject<Unit>());
+        }
+
+        private Subject<Unit> onLeftLobby;
+
+        public void OnLeftLobby()
+        {
+            onLeftLobby?.OnNext(Unit.Default);
+        }
+
+        public IObservable<Unit> OnLeftLobbyAsObservable()
+        {
+            return onLeftLobby ?? (onLeftLobby = new Subject<Unit>());
+        }
+
+        private Subject<List<RoomInfo>> onRoomListUpdate;
+
+        public void OnRoomListUpdate(List<RoomInfo> roomList)
+        {
+            onRoomListUpdate?.OnNext(roomList);
+        }
+
+        public IObservable<List<RoomInfo>> OnRoomListUpdateAsObservable()
+        {
+            return onRoomListUpdate ?? (onRoomListUpdate = new Subject<List<RoomInfo>>());
+        }
+
+        private Subject<List<TypedLobbyInfo>> onLobbyStatisticsUpdate;
+
+        public void OnLobbyStatisticsUpdate(List<TypedLobbyInfo> lobbyStatistics)
+        {
+            onLobbyStatisticsUpdate?.OnNext(lobbyStatistics);
+        }
+
+        public IObservable<List<TypedLobbyInfo>> OnLobbyStatisticsUpdateAsObservable()
+        {
+            return onLobbyStatisticsUpdate ?? (onLobbyStatisticsUpdate = new Subject<List<TypedLobbyInfo>>());
+        }
+
+        #region lifecycle
+
+        private void OnEnable()
+        {
+            PhotonNetwork.AddCallbackTarget(this);
+        }
+
+        private void OnDisable()
+        {
+            PhotonNetwork.RemoveCallbackTarget(this);
+        }
+
+        #endregion
+
+        #region UniRx
+
+        protected override void RaiseOnCompletedOnDestroy()
+        {
+            onJoinedLobby?.OnCompleted();
+            onLeftLobby?.OnCompleted();
+            onRoomListUpdate?.OnCompleted();
+            onLobbyStatisticsUpdate?.OnCompleted();
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/PUN2Rx/Plugins/Triggers/LobbyCallbacksTriggersExtension.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Photon.Realtime;
+using UniRx;
+using UnityEngine;
+
+namespace PUN2Rx
+{
+    public static class LobbyCallbacksTriggersExtension
+    {
+        public static IObservable<Unit> OnJoinedLobbyAsObservable(this Component component)
+        {
+            return component?.gameObject == null
+                ? Observable.Empty<Unit>()
+                : GetOrAddComponent<LobbyCallbacksTriggers>(component.gameObject).OnJoinedLobbyAsObservable();
+        }
+
+        public static IObservable<Unit> OnLeftLobbyAsObservable(this Component component)
+        {
+            return component?.gameObject == null
+                ? Observable.Empty<Unit>()
+                : GetOrAddComponent<LobbyCallbacksTriggers>(component.gameObject).OnLeftRoomAsObservable();
+        }
+
+        public static IObservable<List<RoomInfo>> OnRoomListUpdateAsObservable(this Component component)
+        {
+            return component?.gameObject == null
+                ? Observable.Empty<List<RoomInfo>>()
+                : GetOrAddComponent<LobbyCallbacksTriggers>(component.gameObject).OnRoomListUpdateAsObservable();
+        }
+
+        public static IObservable<List<TypedLobbyInfo>> OnLobbyStatisticsUpdateAsObservable(this Component component)
+        {
+            return component?.gameObject == null
+                ? Observable.Empty<List<TypedLobbyInfo>>()
+                : GetOrAddComponent<LobbyCallbacksTriggers>(component.gameObject).OnLobbyStatisticsUpdateAsObservable();
+        }
+
+        private static T GetOrAddComponent<T>(GameObject gameObject)
+            where T : Component
+        {
+            var component = gameObject.GetComponent<T>();
+            if (component == null)
+            {
+                component = gameObject.AddComponent<T>();
+            }
+
+            return component;
+        }
+    }
+}
