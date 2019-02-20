ed71b7613ea76e031c68afce583d45dc901102cf
nekomimi-daimao <>
2019-02-20 23:40:37 +0900

    implement IConnectionCallbacks

---
+++ Assets/PUN2Rx/Plugins/Triggers/ConnectionCallbacksTriggers.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace PUN2Rx
+{
+    [DisallowMultipleComponent]
+    public class ConnectionCallbacksTriggers : ObservableTriggerBase, IConnectionCallbacks
+    {
+        #region PUN2Callbacks
+
+        private Subject<Unit> onConnected;
+
+        public void OnConnected()
+        {
+            onConnected?.OnNext(Unit.Default);
+        }
+
+        public IObservable<Unit> OnConnectedAsObservable()
+        {
+            return onConnected ?? (onConnected = new Subject<Unit>());
+        }
+
+        private Subject<Unit> onConnectedToMaster;
+
+        public void OnConnectedToMaster()
+        {
+            onConnectedToMaster?.OnNext(Unit.Default);
+        }
+
+        public IObservable<Unit> OnConnectedToMasterAsObservable()
+        {
+            return onConnectedToMaster ?? (onConnectedToMaster = new Subject<Unit>());
+        }
+
+        private Subject<DisconnectCause> onDisconnected;
+
+        public void OnDisconnected(DisconnectCause cause)
+        {
+            onDisconnected?.OnNext(cause);
+        }
+
+        public IObservable<DisconnectCause> OnDisconnectedAsObservable()
+        {
+            return onDisconnected ?? (onDisconnected = new Subject<DisconnectCause>());
+        }
+
+        private Subject<RegionHandler> onRegionListReceived;
+
+        public void OnRegionListReceived(RegionHandler regionHandler)
+        {
+            onRegionListReceived?.OnNext(regionHandler);
+        }
+
+        public IObservable<RegionHandler> OnRegionListReceivedAsObservable()
+        {
+            return onRegionListReceived ?? (onRegionListReceived = new Subject<RegionHandler>());
+        }
+
+        private Subject<Dictionary<string, object>> onCustomAuthenticationResponse;
+
+        public void OnCustomAuthenticationResponse(Dictionary<string, object> data)
+        {
+            onCustomAuthenticationResponse?.OnNext(data);
+        }
+
+        public IObservable<Dictionary<string, object>> OnCustomAuthenticationResponseAsObservable()
+        {
+            return onCustomAuthenticationResponse ?? (onCustomAuthenticationResponse = new Subject<Dictionary<string, object>>());
+        }
+
+        private Subject<string> onCustomAuthenticationFailed;
+
+        public void OnCustomAuthenticationFailed(string debugMessage)
+        {
+            onCustomAuthenticationFailed?.OnError(new PUN2Exception(debugMessage));
+        }
+
+        public IObservable<string> OnCustomAuthenticationFailedAsObservable()
+        {
+            return onCustomAuthenticationFailed ?? (onCustomAuthenticationFailed = new Subject<string>());
+        }
+
+        #endregion
+
+
+        #region lifecycle
+
+        private void OnEnable()
+        {
+            PhotonNetwork.AddCallbackTarget(this);
+        }
+
+        private void OnDisable()
+        {
+            PhotonNetwork.RemoveCallbackTarget(this);
+        }
+
+        #endregion
+
+        #region UniRx
+
+        protected override void RaiseOnCompletedOnDestroy()
+        {
+            onConnected?.OnCompleted();
+            onConnectedToMaster?.OnCompleted();
+            onDisconnected?.OnCompleted();
+            onRegionListReceived?.OnCompleted();
+            onCustomAuthenticationResponse?.OnCompleted();
+            onCustomAuthenticationFailed?.OnCompleted();
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/PUN2Rx/Plugins/Triggers/ConnectionCallbacksTriggersExtension.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Photon.Realtime;
+using UniRx;
+using UnityEngine;
+
+namespace PUN2Rx
+{
+    public static class ConnectionCallbacksTriggersExtension
+    {
+        public static IObservable<Unit> OnConnectedAsObservable(this Component component)
+        {
+            return component?.gameObject == null
+                ? Observable.Empty<Unit>()
+                : GetOrAddComponent<ConnectionCallbacksTriggers>(component.gameObject).OnConnectedAsObservable();
+        }
+
+        public static IObservable<Unit> OnConnectedToMasterAsObservable(this Component component)
+        {
+            return component?.gameObject == null
+                ? Observable.Empty<Unit>()
+                : GetOrAddComponent<ConnectionCallbacksTriggers>(component.gameObject).OnConnectedToMasterAsObservable();
+        }
+
+        public static IObservable<DisconnectCause> OnDisconnectedAsObservable(this Component component)
+        {
+            return component?.gameObject == null
+                ? Observable.Empty<DisconnectCause>()
+                : GetOrAddComponent<ConnectionCallbacksTriggers>(component.gameObject).OnDisconnectedAsObservable();
+        }
+
+        public static IObservable<RegionHandler> OnRegionListReceivedAsObservable(this Component component)
+        {
+            return component?.gameObject == null
+                ? Observable.Empty<RegionHandler>()
+                : GetOrAddComponent<ConnectionCallbacksTriggers>(component.gameObject).OnRegionListReceivedAsObservable();
+        }
+
+        public static IObservable<Dictionary<string, object>> OnCustomAuthenticationResponseAsObservable(this Component component)
+        {
+            return component?.gameObject == null
+                ? Observable.Empty<Dictionary<string, object>>()
+                : GetOrAddComponent<ConnectionCallbacksTriggers>(component.gameObject).OnCustomAuthenticationResponseAsObservable();
+        }
+
+        public static IObservable<string> OnCustomAuthenticationFailedAsObservable(this Component component)
+        {
+            return component?.gameObject == null
+                ? Observable.Empty<string>()
+                : GetOrAddComponent<ConnectionCallbacksTriggers>(component.gameObject).OnCustomAuthenticationFailedAsObservable();
+        }
+
+
+        private static T GetOrAddComponent<T>(GameObject gameObject)
+            where T : Component
+        {
+            var component = gameObject.GetComponent<T>();
+            if (component == null)
+            {
+                component = gameObject.AddComponent<T>();
+            }
+
+            return component;
+        }
+    }
+}
