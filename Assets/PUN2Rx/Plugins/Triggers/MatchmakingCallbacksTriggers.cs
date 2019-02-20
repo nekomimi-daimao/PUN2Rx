1fe1146581710657e07991e50a1e437c27619f79
nekomimi-daimao <>
2019-02-21 00:41:04 +0900

    implement IMatchmakingCallbacks

---
+++ Assets/PUN2Rx/Plugins/Triggers/MatchmakingCallbacksTriggers.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace PUN2Rx
+{
+    [DisallowMultipleComponent]
+    public class MatchmakingCallbacksTriggers : ObservableTriggerBase, IMatchmakingCallbacks
+    {
+        #region PUNCallbacks
+
+        private Subject<List<FriendInfo>> onFriendListUpdate;
+
+        public void OnFriendListUpdate(List<FriendInfo> friendList)
+        {
+            onFriendListUpdate?.OnNext(friendList);
+        }
+
+        public IObservable<List<FriendInfo>> OnFriendListUpdateAsObservable()
+        {
+            return onFriendListUpdate ?? (onFriendListUpdate = new Subject<List<FriendInfo>>());
+        }
+
+        private Subject<Unit> onCreatedRoom;
+
+        public void OnCreatedRoom()
+        {
+            onCreatedRoom?.OnNext(Unit.Default);
+        }
+
+        public IObservable<Unit> OnCreatedRoomAsObservable()
+        {
+            return onCreatedRoom ?? (onCreatedRoom = new Subject<Unit>());
+        }
+
+        private Subject<Unit> onCreateRoomFailed;
+
+        public void OnCreateRoomFailed(short returnCode, string message)
+        {
+            onCreateRoomFailed?.OnError(new PUN2Exception(returnCode, message));
+        }
+
+        public IObservable<Unit> OnCreateRoomFailedAsObservable()
+        {
+            return onCreateRoomFailed ?? (onCreateRoomFailed = new Subject<Unit>());
+        }
+
+        private Subject<Unit> onJoinedRoom;
+
+        public void OnJoinedRoom()
+        {
+            onJoinedRoom?.OnNext(Unit.Default);
+        }
+
+        public IObservable<Unit> OnJoinedRoomAsObservable()
+        {
+            return onJoinedRoom ?? (onJoinedRoom = new Subject<Unit>());
+        }
+
+        private Subject<Unit> onJoinRoomFailed;
+
+        public void OnJoinRoomFailed(short returnCode, string message)
+        {
+            onJoinRoomFailed?.OnError(new PUN2Exception(returnCode, message));
+        }
+
+        public IObservable<Unit> OnJoinRoomFailedAsObservable()
+        {
+            return onJoinRoomFailed ?? (onJoinRoomFailed = new Subject<Unit>());
+        }
+
+        private Subject<Unit> onJoinRandomFailed;
+
+        public void OnJoinRandomFailed(short returnCode, string message)
+        {
+            onJoinRandomFailed?.OnError(new PUN2Exception(returnCode, message));
+        }
+
+        public IObservable<Unit> OnJoinRandomFailedAsObservable()
+        {
+            return onJoinRandomFailed ?? (onJoinRandomFailed = new Subject<Unit>());
+        }
+
+        private Subject<Unit> onLeftRoom;
+
+        public void OnLeftRoom()
+        {
+            onLeftRoom?.OnNext(Unit.Default);
+        }
+
+        public IObservable<Unit> OnLeftRoomAsObservable()
+        {
+            return onLeftRoom ?? (onLeftRoom = new Subject<Unit>());
+        }
+
+        #endregion
+
+        #region lifecycle
+
+        private void OnEnable()
+        {
+            PhotonNetwork.AddCallbackTarget(this);
+        }
+
+        private void OnDisable()
+        {
+            PhotonNetwork.RemoveCallbackTarget(this);
+        }
+
+        #endregion
+
+        #region UniRx
+
+        protected override void RaiseOnCompletedOnDestroy()
+        {
+            onFriendListUpdate?.OnCompleted();
+            onCreatedRoom?.OnCompleted();
+            onCreateRoomFailed?.OnCompleted();
+            onJoinedRoom?.OnCompleted();
+            onJoinRoomFailed?.OnCompleted();
+            onJoinRandomFailed?.OnCompleted();
+            onLeftRoom?.OnCompleted();
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/PUN2Rx/Plugins/Triggers/MatchmakingCallbacksTriggersExtension.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using Photon.Realtime;
+using UniRx;
+using UnityEngine;
+
+namespace PUN2Rx
+{
+    public static class MatchmakingCallbacksTriggersExtension
+    {
+        public static IObservable<List<FriendInfo>> OnFriendListUpdateAsObservable(this Component component)
+        {
+            return component?.gameObject == null
+                ? Observable.Empty<List<FriendInfo>>()
+                : GetOrAddComponent<MatchmakingCallbacksTriggers>(component.gameObject).OnFriendListUpdateAsObservable();
+        }
+
+        public static IObservable<Unit> OnCreateRoomAsObservable(this Component component)
+        {
+            return component?.gameObject == null
+                ? Observable.Empty<Unit>()
+                : GetOrAddComponent<MatchmakingCallbacksTriggers>(component.gameObject).OnCreatedRoomAsObservable();
+        }
+
+        public static IObservable<Unit> OnCreateRoomFailedAsObservable(this Component component)
+        {
+            return component?.gameObject == null
+                ? Observable.Empty<Unit>()
+                : GetOrAddComponent<MatchmakingCallbacksTriggers>(component.gameObject).OnCreateRoomFailedAsObservable();
+        }
+
+        public static IObservable<Unit> OnJoinedRoomAsObservable(this Component component)
+        {
+            return component?.gameObject == null
+                ? Observable.Empty<Unit>()
+                : GetOrAddComponent<MatchmakingCallbacksTriggers>(component.gameObject).OnJoinedRoomAsObservable();
+        }
+
+        public static IObservable<Unit> OnJoinRoomFailedAsObservable(this Component component)
+        {
+            return component?.gameObject == null
+                ? Observable.Empty<Unit>()
+                : GetOrAddComponent<MatchmakingCallbacksTriggers>(component.gameObject).OnJoinRoomFailedAsObservable();
+        }
+
+        public static IObservable<Unit> OnJoinRandomFailedAsObservable(this Component component)
+        {
+            return component?.gameObject == null
+                ? Observable.Empty<Unit>()
+                : GetOrAddComponent<MatchmakingCallbacksTriggers>(component.gameObject).OnJoinRandomFailedAsObservable();
+        }
+
+        public static IObservable<Unit> OnLeftRoomAsObservable(this Component component)
+        {
+            return component?.gameObject == null
+                ? Observable.Empty<Unit>()
+                : GetOrAddComponent<MatchmakingCallbacksTriggers>(component.gameObject).OnLeftRoomAsObservable();
+        }
+
+        private static T GetOrAddComponent<T>(GameObject gameObject)
+            where T : Component
+        {
+            var component = gameObject.GetComponent<T>();
+            if (component == null)
+            {
+                component = gameObject.AddComponent<T>();
+            }
+
+            return component;
+        }
+    }
+}
