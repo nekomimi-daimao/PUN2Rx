f3b21b547de730350aa46b55e465d64da5f78083
nekomimi-daimao <>
2019-02-23 02:17:52 +0900

    implement IPunOwnershipCallbacks

---
+++ Assets/PUN2Rx/Plugins/Triggers/PunOwnershipCallbacksTriggers.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Photon.Pun;
+using Photon.Realtime;
+using UniRx;
+using UniRx.Triggers;
+
+namespace PUN2Rx
+{
+    public class PunOwnershipCallbacksTriggers : ObservableTriggerBase, IPunOwnershipCallbacks
+    {
+        #region PUN2Callbacks
+
+        private Subject<Tuple<PhotonView, Player>> onOwnershipRequest;
+
+        public void OnOwnershipRequest(PhotonView targetView, Player requestingPlayer)
+        {
+            onOwnershipRequest?.OnNext(Tuple.Create<PhotonView, Player>(targetView, requestingPlayer));
+        }
+
+        public IObservable<Tuple<PhotonView, Player>> OnOwnershipRequestAsObservable()
+        {
+            return onOwnershipRequest ?? (onOwnershipRequest = new Subject<Tuple<PhotonView, Player>>());
+        }
+
+        private Subject<Tuple<PhotonView, Player>> onOwnershipTransferred;
+
+        // TODO ...typo? Transferred?
+        public void OnOwnershipTransfered(PhotonView targetView, Player previousOwner)
+        {
+            onOwnershipTransferred?.OnNext(Tuple.Create(targetView, previousOwner));
+        }
+
+        public IObservable<Tuple<PhotonView, Player>> OnOwnershipTransferredAsObservable()
+        {
+            return onOwnershipTransferred ?? (onOwnershipTransferred = new Subject<Tuple<PhotonView, Player>>());
+        }
+
+        #endregion
+
+        #region lifecycle
+
+        private void OnEnable()
+        {
+            PhotonNetwork.AddCallbackTarget(this);
+        }
+
+        private void OnDisable()
+        {
+            PhotonNetwork.RemoveCallbackTarget(this);
+        }
+
+        #endregion
+
+        #region UniRx
+
+        protected override void RaiseOnCompletedOnDestroy()
+        {
+            onOwnershipRequest?.OnCompleted();
+            onOwnershipTransferred?.OnCompleted();
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/PUN2Rx/Plugins/Triggers/PunOwnershipCallbacksTriggersExtension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Photon.Pun;
+using Photon.Realtime;
+using UniRx;
+using UnityEngine;
+
+namespace PUN2Rx
+{
+    public static class PunOwnershipCallbacksTriggersExtension
+    {
+        public static IObservable<Tuple<PhotonView, Player>> OnOwnershipRequestAsObservable(this Component component)
+        {
+            return component?.gameObject == null
+                ? Observable.Empty<Tuple<PhotonView, Player>>()
+                : GetOrAddComponent<PunOwnershipCallbacksTriggers>(component.gameObject).OnOwnershipRequestAsObservable();
+        }
+
+        public static IObservable<Tuple<PhotonView, Player>> OnOwnershipTransferredAsObservable(this Component component)
+        {
+            return component?.gameObject == null
+                ? Observable.Empty<Tuple<PhotonView, Player>>()
+                : GetOrAddComponent<PunOwnershipCallbacksTriggers>(component.gameObject).OnOwnershipTransferredAsObservable();
+        }
+
+        private static T GetOrAddComponent<T>(GameObject gameObject)
+            where T : Component
+        {
+            var component = gameObject.GetComponent<T>();
+            if (component == null)
+            {
+                component = gameObject.AddComponent<T>();
+            }
+
+            return component;
+        }
+    }
+}
